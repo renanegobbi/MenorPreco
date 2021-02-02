a85fbb6994d0348c3fd550391d6b1d87cff38d90
renanegobbi <renaneg2019@hotmail.com>
2021-02-01 21:53:03 -0300

    Adiconando a string de conexão.

--- MenorPreco/MenorPreco.Api/Startup.cs
+++ MenorPreco/MenorPreco.Api/Startup.cs
@@ -1,6 +1,9 @@
+using MenorPreco.DAL.Estabelecimentos;
+using MenorPreco.Model;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,24 +28,29 @@ namespace MenorPreco.Api
         {
             services.AddApiVersioning();
 
-            services.AddControllersWithViews();
+            //services.AddDbContext<MenorPrecoContext>(options =>
+            //    options.UseSqlServer(Configuration.GetConnectionString("SqlServerConnectionString")));
+            services.AddDbContext<MenorPrecoContext>(options =>
+               options.UseSqlite(Configuration.GetConnectionString("SqLiteConnectionString")));
+
+            services.AddTransient<IRepository<Produto>, RepositorioBaseEF<Produto>>();
+            services.AddTransient<IRepository<Estabelecimento>, RepositorioBaseEF<Estabelecimento>>();
+            services.AddTransient<IRepository<Venda>, RepositorioBaseEF<Venda>>();
+            services.AddTransient<IRepository<VendaProduto>, RepositorioBaseEF<VendaProduto>>();
+
+            //services.AddControllersWithViews();
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
 
             app.UseRouting();
 
@@ -50,10 +58,14 @@ namespace MenorPreco.Api
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllers();
             });
+
+            serviceProvider
+                .GetService<MenorPrecoContext>().Database.EnsureCreated();
+
+            serviceProvider
+                .GetService<MenorPrecoContext>().Database.Migrate();
         }
     }
 }
