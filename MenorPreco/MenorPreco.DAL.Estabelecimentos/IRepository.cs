9e27a5687d45e88b38c67f53e701a36e72f37338
renanegobbi <renaneg2019@hotmail.com>
2021-02-01 21:41:49 -0300

    Incluindo o repositório.

---
+++ MenorPreco/MenorPreco.DAL.Estabelecimentos/IRepository.cs
@@ -0,0 +1,26 @@
+﻿using MenorPreco.Model;
+using System.Linq;
+
+namespace MenorPreco.DAL.Estabelecimentos
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        TEntity ProcurarPeloEstabelecimento(int key);
+
+        void IncluirEstabelecimento(Estabelecimento estabelecimento);
+
+        void SalvaProduto();
+        void InclueProduto(Produto produto);
+
+        TEntity ProcurarPorProduto(long key);
+
+        void InclueVenda(Venda venda);
+
+        void InclueVendaProduto(VendaProduto vendaProduto);
+
+        IQueryable<TEntity> All { get; }
+
+        IQueryable<Produto> InclueEstabelecimento { get; }
+    }
+}
+
---
+++ MenorPreco/MenorPreco.DAL.Estabelecimentos/RepositorioBaseEF.cs
@@ -0,0 +1,39 @@
+﻿using MenorPreco.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace MenorPreco.DAL.Estabelecimentos
+{
+    public class RepositorioBaseEF<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private readonly MenorPrecoContext _context;
+
+        public RepositorioBaseEF(MenorPrecoContext context)
+        {
+            _context = context;
+        }
+
+        public TEntity ProcurarPeloEstabelecimento(int key)
+        {
+            return _context.Find<TEntity>(key);
+        }
+
+        public void IncluirEstabelecimento(Estabelecimento estabelecimento) => _context.Set<Estabelecimento>().Add(estabelecimento);
+
+        public void SalvaProduto() => _context.SaveChanges();
+        public void InclueProduto(Produto produto) => _context.Set<Produto>().Add(produto);
+
+        public TEntity ProcurarPorProduto(long key)
+        {
+            return _context.Find<TEntity>(key);
+        }
+
+        public void InclueVenda(Venda venda) => _context.Set<Venda>().Add(venda);
+
+        public void InclueVendaProduto(VendaProduto vendaProduto) => _context.Set<VendaProduto>().Add(vendaProduto);
+
+        public IQueryable<TEntity> All => _context.Set<TEntity>().AsQueryable();
+
+        public IQueryable<Produto> InclueEstabelecimento => _context.Set<Produto>().Include(x => x.ESTABELECIMENTO);
+    }
+}
